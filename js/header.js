e4613c929b4384ea244c66bab2fb11f8c499e2bb
Artem Uholkov <artem.ugolkov@ukr.net>
2025-07-10 13:11:24 +0300

    test

--- js/header.js
+++ js/header.js
@@ -50,3 +50,52 @@ function burgerHandler(e) {
   }
   burger.classList.toggle('active');
 }
+
+const bottles = document.querySelectorAll('.invest_bottle');
+const bottlesContainer = document.querySelector('.invest_bottles');
+const productsContainer = document.querySelector('.products');
+
+bottles.forEach((bottle) => {
+  bottle.addEventListener('click', () => {
+    // Check if the clicked bottle is already active
+    const isAlreadyClicked = bottle.classList.contains('clicked');
+
+    // Remove clicked class from all bottles
+    bottles.forEach((b) => {
+      b.classList.remove('clicked');
+    });
+
+    // Remove all bottle-specific classes from products container
+    productsContainer.classList.remove(
+      'bottle1-active',
+      'bottle2-active',
+      'bottle3-active',
+      'bottle4-active',
+      'has-active-content',
+    );
+
+    // If the bottle wasn't already clicked, activate it
+    if (!isAlreadyClicked) {
+      bottle.classList.add('clicked');
+
+      // Add specific class based on which bottle was clicked
+      if (bottle.classList.contains('bottle1')) {
+        productsContainer.classList.add('bottle1-active', 'has-active-content');
+      } else if (bottle.classList.contains('bottle2')) {
+        productsContainer.classList.add('bottle2-active', 'has-active-content');
+      } else if (bottle.classList.contains('bottle3')) {
+        productsContainer.classList.add('bottle3-active', 'has-active-content');
+      } else if (bottle.classList.contains('bottle4')) {
+        productsContainer.classList.add('bottle4-active', 'has-active-content');
+      }
+    }
+
+    // Update container class based on whether any bottle is active
+    const hasActiveBottle = document.querySelector('.invest_bottle.clicked');
+    if (hasActiveBottle) {
+      bottlesContainer.classList.add('has-active');
+    } else {
+      bottlesContainer.classList.remove('has-active');
+    }
+  });
+});
